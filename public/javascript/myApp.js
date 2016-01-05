54e0bf8af797ebfc06a14a7aa9be2d7fae952fde
Anthony Dell'Anno <anthony@dellanno.co.uk>
2016-01-05 15:03:14 +0000

    mongo db database setup & working

--- public/javascript/myApp.js
+++ public/javascript/myApp.js
@@ -5,4 +5,10 @@ myApp.controller('AppCtrl', [
   '$http',
   function($scope, $http) {
     console.log("Hello World from controller");
+
+    $http.get('/contactlist').success(function(response) {
+      console.log("I got the data I requested");
+      $scope.contactlist = response; 
+    });
+
 }]);
