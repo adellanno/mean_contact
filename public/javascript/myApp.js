b6f1ba14bb835ea4fb9f9dde623e5da724186d67
Anthony Dell'Anno <anthony@dellanno.co.uk>
2016-01-05 17:11:12 +0000

    can now edit/update contacts

--- public/javascript/myApp.js
+++ public/javascript/myApp.js
@@ -27,10 +27,23 @@ myApp.controller('AppCtrl', [
 
   $scope.remove = function(id) {
     console.log(id);
-    $http.delete('/contactlist/' + id).success(function(resonse) {
+    $http.delete('/contactlist/' + id).success(function(response) {
+      refresh();
+    });
+  };
+
+  $scope.edit = function(id) {
+    console.log(id);
+    $http.get('/contactlist/' + id).success(function(response) {
+      $scope.contact = response; // puts the edited response back into the contact
+    });
+  };
+
+  $scope.update = function() {
+    console.log($scope.contact._id);
+    $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
       refresh();
     })
-
   };
 
 }]);
