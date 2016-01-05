0a87baf77f1ec91a452eaf6b825c19f31b34e7fb
Anthony Dell'Anno <anthony@dellanno.co.uk>
2016-01-05 15:26:13 +0000

    contacts now added to database/refresh automatically on page

--- public/javascript/myApp.js
+++ public/javascript/myApp.js
@@ -6,9 +6,23 @@ myApp.controller('AppCtrl', [
   function($scope, $http) {
     console.log("Hello World from controller");
 
+
+  var refresh = function() {
     $http.get('/contactlist').success(function(response) {
       console.log("I got the data I requested");
-      $scope.contactlist = response; 
+      $scope.contactlist = response;
+      $scope.contact = "";
     });
+  };
+
+  refresh();
+
+  $scope.addContact = function() {
+    console.log($scope.contact)
+    $http.post('/contactlist', $scope.contact).success(function(response) {
+      console.log(response);
+      refresh();
+    });       // this sends our data to the database
+  };
 
 }]);
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactlist', ['contactlist']); // which database are we using
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public")); // looks for static(html) files for the views
+app.use(bodyParser.json());
 
 app.get('/contactlist', function (req, res) {
   console.log("I received a get request")
@@ -13,6 +15,13 @@ app.get('/contactlist', function (req, res) {
     res.json(docs);
   })
 });
+
+app.post('/contactlist', function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  })
+})
 
 app.listen(3000);
 console.log("Server running on port 3000");
